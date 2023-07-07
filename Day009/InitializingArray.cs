c46b40fc73a6d61ca9aa0e36c8c10b670ae1ed6f
rlarhksdnrlarhksdn <137870369+rlarhksdnrlarhksdn@users.noreply.github.com>
2023-07-07 09:49:57 +0900

    Create InitializingArray.cs

---
+++ Day009/InitializingArray.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitializingArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //배열을 초기화하는 세가지 방법
+            string[] array1 = new string[3] { "안녕", "Hello", "Halo" };
+            //선언후 초기화
+            array1[0] = "반갑습니다.";
+            array1[1]= "Hello";
+            array1[2] = "Halo";
+
+            //배열의 용량을 생략가능
+            string[] array2 = new string[] { "안녕", "Hello", "Halo" };
+            //new 생략
+            string[] array3 = { "안녕", "Hello", "Halo" };
+            foreach (string s in array3)
+                Console.WriteLine(s);
+
+            int[] arr1 = new int[3] { 1, 2, 3 };
+            int[] arr2 = new int[] { 1, 2, 3 };
+            int[] arr3 = { 1, 2, 3 }; // 가능하다.
+
+            Console.WriteLine($"type :{arr3.GetType()}");
+            Console.WriteLine($"type :{array3.GetType()}");
+            Console.WriteLine($"type :{arr3.GetType().BaseType}");
+            Console.WriteLine($"type :{array3.GetType().BaseType}");
+
+        }
+    }
+}
