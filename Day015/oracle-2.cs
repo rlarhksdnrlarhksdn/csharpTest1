f10042bb87c18466009c8f2031f6b0b18b2489d5
rlarhksdnrlarhksdn <137870369+rlarhksdnrlarhksdn@users.noreply.github.com>
2023-07-17 17:01:52 +0900

    Create oracle-2.cs

---
+++ Day015/oracle-2.cs
@@ -0,0 +1,41 @@
+#NVL 함수 사용하여 NULL을 변환
+SELECT EMPNO, ENAME, SAL,
+       NVL (COMM,0), SAL+NVL(COMM,0)
+FROM EMP;
+
+#NVL2함수
+SELECT EMPNO, ENAME, COMM,
+        NVL2(COMM,'O','X'),
+        NVL2(COMM, SAL*12+COMM, SAL*12) AS 연봉
+FROM EMP;
+
+#DECODE 상황에 따라 다른 데이터를 반환하는 함수
+SELECT EMPNO, ENAME, JOB, SAL,
+       DECODE(JOB, 'MANAGER', SAL*1.1,
+                   'SALESMAN',SAL*1.05,
+       SAL*1)AS UPSAL
+FROM EMP;
+
+#JOB이 'PRESIDENT'일때 MGR 8000 한다면?
+SELECT EMPNO, ENAME, JOB,
+       DECODE(JOB,'PRESIDENT',8000,0)
+FROM EMP;
+
+#CASE문
+SELECT EMPNO, ENAME, JOB, SAL,
+      CASE JOB
+            WHEN 'MANAGER' THEN SAL*1.1
+            WHEN 'SALESMAN' THEN SAL*1.05
+            ELSE SAL*1.03
+      END AS UPSAL
+FROM EMP;
+
+#기준 데이터 없이 조건식만으로 CASE문 사용하기
+#열 값에 따라 출력값이 달라지는 CASE
+SELECT EMPNO, ENAME, COMM,
+    CASE
+        WHEN COMM IS NULL THEN '해당사항 없음'
+        WHEN COMM = 0 THEN '수당 없음'
+        WHEN COMM > 0 THEN '수당 : '|| COMM
+    END AS COMM_TEXT
+FROM EMP;
