75bc3a0d0dc4568819b9f9e90d36624d84d56151
rlarhksdnrlarhksdn <137870369+rlarhksdnrlarhksdn@users.noreply.github.com>
2023-09-18 15:21:59 +0900

    Create 리눅스 기본 명령어.cs

---
+++ 리눅스/리눅스 기본 명령어.cs
@@ -0,0 +1,19 @@
+Linux Commands
+연습할 파일 생성
+pi@raspberrypi ~ $ ls > sample.txt
+ls 명령에 의한 출력결과를 sample.txt로 보냄
+pi@raspberrypi ~ $ cat sample.txt
+복사 
+pi@raspberrypi ~ $ mkdir workspace
+복사
+pi@raspberrypi ~ $ cp ./sample.txt ./workspace
+pi@raspberrypi ~ $ ls
+확인
+pi@raspberrypi ~ $ cd workspace
+	pi@raspberrypi ~/worksapce $ ls
+삭제
+pi@raspberrypi ~/worksapce $ rm sample.txt
+pi@raspberrypi ~/worksapce $ ls
+pi@raspberrypi ~/worksapce $ cd
+pi@raspberrypi ~ $
+
